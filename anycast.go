c84a34ec7a93159a79ea5c067934e62fb7ba9f04
Aitugan <43519581+Aitugan@users.noreply.github.com>
2019-08-12 05:02:32 +0600

    Update anycast.go

--- anycast.go
+++ anycast.go
@@ -1,1 +1,20 @@
 package main
+
+func AnycastRunner(muxer *mux.Router, interf, path, method string, upstream Upstream) {
+	muxer.HandleFunc(path, upstream.anycastHandler).Methods(method)
+
+}
+
+func (upstream Upstream) anycastHandler(w http.ResponseWriter, r *http.Request) {
+
+	mainCH := make(chan []byte, 1)
+
+	for _, backend := range upstream.Backends {
+		go func(url string, ch chan){
+			ch <- sendRequest(url)
+		}(backend,mainCH)//sendRequestToChannel(backend, fibonacciNumbersResultChannel)
+	}
+
+	w.Write(<-fibonacciNumbersResultChannel)
+
+}
