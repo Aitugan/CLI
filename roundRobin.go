a16b5a4d88087fd587eb38556d50b36b52bfa934
Aitugan <43519581+Aitugan@users.noreply.github.com>
2019-08-12 04:02:59 +0600

    Update roundRobin.go

--- roundRobin.go
+++ roundRobin.go
@@ -1,31 +1,46 @@
 package main
 
 import (
-	"net/url"
-	"sync"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type RoundRobin interface {
-	Next() *url.URL
+type UpstreamNumber struct {
+	Upstream
+	Count int
 }
 
-type Roundrobin struct {
-	urls []*url.URL
-	mu   *sync.Mutex
-	next int
+type RoundRobin struct{}
+
+func (rr *RoundRobin) RoundRobinRunner(interf, path, method string, upstream Upstream) {
+	r := mux.NewRouter()
+	upNum := UpstreamNumber{upstream, 0}
+	r.HandleFunc(path, upNum.roundRobinHandle).Methods(method)
+	http.ListenAndServe(interf, r)
 }
 
-func New(urls []*url.URL) (RoundRobin) {
-	return &Roundrobin{
-		urls: urls,
-		mu:   new(sync.Mutex),
+func (upstream *UpstreamNumber) roundRobinHandle(w http.ResponseWriter, r *http.Request) {
+
+	w.Write(sendRequest(upstream.Backends[upstream.Count]))
+
+	upstream.Count = (upstream.Count + 1) % len(upstream.Backends)
+
+}
+func sendRequest(url string) []byte {
+
+	response, err := http.Get(url)
+
+	// Check(err)
+	if err != nil {
+		panic(err)
 	}
-}
-
-func (r *Roundrobin) Next() *url.URL {
-	r.mu.Lock()
-	sc := r.urls[r.next]
-	r.next = (r.next + 1) % len(r.urls)
-	r.mu.Unlock()
-	return sc
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	// Check(err)
+	if err != nil {
+		panic(err)
+	}
+	return body
 }
