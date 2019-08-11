3afd0561b21570c1282fb21925e946e1830c8065
Aitugan <43519581+Aitugan@users.noreply.github.com>
2019-08-12 04:04:00 +0600

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/urfave/cli"
 )
 
@@ -42,16 +44,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	filename := "defaultConfig.json"
+	filename := "config.json"
 	config, err := LoadConfiguration(filename)
 	Check(err)
 	fmt.Println(config.Upstreams[0].Path)
 
 	app := cli.NewApp()
 	app.Name = "CLI"
-	// app.Usage = "Idk, just a cli"
-	// app.Description = "For support, contact ..email.."
-	// app.UsageText = ""
+	app.Usage = "Idk, just a cli"
+	app.Description = "For support, contact ..email.."
+	app.UsageText = ""
 	app.Version = "1.0.0"
 	app.Authors = []cli.Author{
 		cli.Author{
@@ -67,16 +69,9 @@ func main() {
 		},
 	}
 	app.Action = func(c *cli.Context) error {
-		// if c.NArg() > 0 {
-		filename = c.String("configFile") //c.Args().Get(0)
-		// }
-		// fmt.Println(c.Args().Get(0))
-		// fmt.Println(c.String("configFile"))
-		// fmt.Println(filename)
-		// fmt.Println("The file ", filename, " will be used as a config file")
+		filename = c.String("configFile")
 		return nil
 	}
-	// config := Config{}
 	app.Commands = []cli.Command{
 		{
 			Name:  "run",
@@ -84,28 +79,64 @@ func main() {
 			Action: func(c *cli.Context) error {
 				fmt.Println("Server is running\n")
 				fmt.Printf("%v will be used as configuration file. If you changed your opinion, use reload command\n\n", filename)
-				//TODO
-				//Start a new proxy server
-				//open in browser... I suppose
+				// app.Flags[0]
 
-				app.Flags[0]
-
-				http.HandleFunc("/", handler)
-				http.ListenAndServe(config.Interface, nil)
+				config, _ := LoadConfiguration("config.json")
+				for i := 0; i < len(config.Upstreams); i++ {
+					RoundRobinRunner(config.Interface, config.Upstreams[i].Path, config.Upstreams[i].Method, config.Upstreams[i])
+				}
 
 				return nil
 			},
 		},
-		// {
-		// 	Name: "reload",
-		// 	Usage: "Reload the proxy server. Stops previous one and opens another with new configuration file"
-		// 	Action: func(c *cli.Context) error {
-		// 		fmt.Println("Server is reloaded")
-		// 		return nil
-		// 	}
-		// }
+		{
+			Name:  "reload",
+			Usage: "Reload the proxy server. Stops previous one and opens another with new configuration file",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Server is reloaded")
+				return nil
+			},
+		},
 	}
 
 	err = app.Run(os.Args)
 	Check(err)
+
+}
+
+///////////////////////////////////////////////////////////////
+type UpstreamNumber struct {
+	Upstream
+	Count int
+}
+
+func RoundRobinRunner(interf, path, method string, upstream Upstream) {
+	r := mux.NewRouter()
+	upNum := UpstreamNumber{upstream, 0}
+	r.HandleFunc(path, upNum.roundRobinHandle).Methods(method)
+	http.ListenAndServe(interf, r)
+}
+
+func (upstream *UpstreamNumber) roundRobinHandle(w http.ResponseWriter, r *http.Request) {
+
+	w.Write(sendRequest(upstream.Backends[upstream.Count]))
+
+	upstream.Count = (upstream.Count + 1) % len(upstream.Backends)
+
+}
+func sendRequest(url string) []byte {
+
+	response, err := http.Get(url)
+
+	// Check(err)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	// Check(err)
+	if err != nil {
+		panic(err)
+	}
+	return body
 }
