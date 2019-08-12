5bb1d0e76cbe0aae6c388634e32762b66e736bd2
Aitugan <43519581+Aitugan@users.noreply.github.com>
2019-08-12 18:48:36 +0600

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	"CLI/proxyMethod"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	"github.com/urfave/cli"
 )
 
@@ -17,37 +14,8 @@ func Check(err error) {
 	}
 }
 
-type Config struct {
-	Interface string     `json:"interface"`
-	Upstreams []Upstream `json:"upstreams"`
-}
-
-type Upstream struct {
-	Path        string   `json:"path"`
-	Method      string   `json:"method"`
-	Backends    []string `json:"backends"`
-	ProxyMethod string   `json:"proxyMethod"`
-}
-
-func LoadConfiguration(file string) (Config, error) {
-	var config Config
-	configFile, err := os.Open(file)
-	defer configFile.Close()
-	Check(err)
-	jsonParser := json.NewDecoder(configFile)
-	jsonParser.Decode(&config)
-	return config, nil
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Some data Some data Some data Some data Some data")
-}
-
 func main() {
-	filename := "config.json"
-	config, err := LoadConfiguration(filename)
-	Check(err)
-
+	filename := "defaultConfig.json"
 	app := cli.NewApp()
 	app.Name = "CLI"
 	app.Usage = "Idk, just a cli"
@@ -71,9 +39,9 @@ func main() {
 		filename = c.String("configFile")
 		return nil
 	}
-	muxer := mux.NewRouter()
 
 	app.Commands = []cli.Command{
+
 		{
 			Name:  "run",
 			Usage: "Run the proxy server",
@@ -81,14 +49,7 @@ func main() {
 				fmt.Println("Server is running\n")
 				fmt.Printf("%v will be used as configuration file. If you changed your opinion, use reload command\n\n", filename)
 
-				for i := 0; i < len(config.Upstreams); i++ {
-					if config.Upstreams[i].ProxyMethod == "round-robin" {
-						RoundRobinRunner(muxer, config.Interface, config.Upstreams[i].Path, config.Upstreams[i].Method, config.Upstreams[i])
-					} else {
-						AnycastRunner(muxer, config.Interface, config.Upstreams[i].Path, config.Upstreams[i].Method, config.Upstreams[i])
-					}
-				}
-				http.ListenAndServe(config.Interface, muxer)
+				proxyMethod.RunServer(filename)
 
 				return nil
 			},
@@ -103,52 +64,7 @@ func main() {
 		},
 	}
 
-	err = app.Run(os.Args)
+	err := app.Run(os.Args)
 	Check(err)
 
-}
-
-type UpstreamNumber struct {
-	Upstream
-	Count int
-}
-
-func RoundRobinRunner(muxer *mux.Router, interf, path, method string, upstream Upstream) {
-	upNum := UpstreamNumber{upstream, 0}
-	muxer.HandleFunc(path, upNum.roundRobinHandle).Methods(method)
-}
-
-func AnycastRunner(muxer *mux.Router, interf, path, method string, upstream Upstream) {
-	muxer.HandleFunc(path, upstream.anycastHandler).Methods(method)
-}
-
-func (upstream *UpstreamNumber) roundRobinHandle(w http.ResponseWriter, r *http.Request) {
-
-	w.Write(sendRequest(upstream.Backends[upstream.Count]))
-
-	upstream.Count = (upstream.Count + 1) % len(upstream.Backends)
-
-}
-
-func (upstream Upstream) anycastHandler(w http.ResponseWriter, r *http.Request) {
-
-	mainCH := make(chan []byte, 1)
-
-	for _, backend := range upstream.Backends {
-		go func(url string, ch chan<- []byte) {
-			ch <- sendRequest(url)
-		}(backend, mainCH)
-	}
-	w.Write(<-mainCH)
-}
-
-func sendRequest(url string) []byte {
-
-	response, err := http.Get(url)
-
-	Check(err)
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	Check(err)
-	return body
 }
